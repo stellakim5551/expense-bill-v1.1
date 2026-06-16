e628d35efba7e7264fe19eaf4353cb585a9754ec
stellakim5551 <stellakim5551@gmail.com>
2026-06-17 01:45:30 +0800

    deploy: 更新静态资源

---
+++ out/_next/static/ltGFx2QEPfoKeKUsaJc0o/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/expense-bill-v1.1//_next/:path+",
+        "destination": "/expense-bill-v1.1/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
